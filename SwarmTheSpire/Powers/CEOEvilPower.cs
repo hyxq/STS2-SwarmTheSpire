d0a6421c00f3e2bc72558e58a20a640a8ea38657
幻雨心晴 <yuanshuhan80318@outlook.com>
2026-05-27 21:17:53 +0800

    新卡牌CEO Evil

---
+++ SwarmTheSpire/Powers/CEOEvilPower.cs
@@ -0,0 +1,58 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace SwarmTheSpire.Powers
+{
+    public sealed class CEOEvilPower : SwarmPowerTemplate
+    {
+        private sealed class Data
+        {
+            public decimal TrackedGold;
+        }
+
+        public override PowerType Type => PowerType.Buff;
+
+        public override PowerStackType StackType => PowerStackType.Single;
+
+        public override string CustomPackedIconPath =>
+            "res://SwarmTheSpire/images/powers/SwarmTheSpire-miles_power.png";
+
+        public override string CustomBigIconPath =>
+            "res://SwarmTheSpire/images/powers/SwarmTheSpire-miles_power.png";
+
+        protected override object InitInternalData() => new Data();
+
+        public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+        {
+            if (cardPlay.Card.Owner.Creature != Owner)
+                return;
+
+            var player = Owner.Player;
+            if (player == null)
+                return;
+
+            var data = GetInternalData<Data>();
+            var currentGold = (decimal)player.Gold;
+
+            if (data.TrackedGold == 0)
+            {
+                data.TrackedGold = currentGold;
+                return;
+            }
+
+            var delta = currentGold - data.TrackedGold;
+            data.TrackedGold = currentGold;
+
+            if (delta != 0)
+            {
+                var gain = delta > 0 ? delta : -delta;
+                Flash();
+                await CreatureCmd.GainBlock(Owner, gain, ValueProp.Unpowered, null);
+            }
+        }
+    }
+}
+
