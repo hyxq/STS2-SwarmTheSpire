d66aded463cb9c3087ae9f69a74925eea172a1fc
幻雨心晴 <yuanshuhan80318@outlook.com>
2026-07-11 03:43:27 +0800

    fix: 同步鱼叉连击本地化key与类名

--- SwarmTheSpire/Cards/Uncommon/IEvilTV.cs
+++ SwarmTheSpire/Cards/Uncommon/IEvilTV.cs
@@ -12,27 +12,19 @@ namespace SwarmTheSpire.Cards
     public sealed class IEvilTV() : SwarmEvilPoolCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self, true)
     {
         protected override IEnumerable<DynamicVar> CanonicalVars =>
-        [new PowerVar<MilesPower>(2m),
-        new PowerVar<VulnerablePower>(1m),
-        new PowerVar<ChargePower>(2m),
-        new BlockVar(11m, ValueProp.Move)];
+        [new PowerVar<VulnerablePower>(1m),
+        new PowerVar<ChargePower>(1m),
+        new BlockVar(13m, ValueProp.Move)];
 
         public override bool GainsBlock => true;
         protected override IEnumerable<IHoverTip> AdditionalHoverTips =>
         [
             HoverTipFactory.FromPower<VulnerablePower>(),
-            HoverTipFactory.FromPower<MilesPower>(),
             HoverTipFactory.FromPower<ChargePower>(),
         ];
 
         protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
         {
-            var combatState = CombatState;
-            ArgumentNullException.ThrowIfNull(combatState);
-            var hittableEnemies = combatState.HittableEnemies;
-            foreach (var item in hittableEnemies)
-            await PowerCmd.Apply<MilesPower>(choiceContext, item, DynamicVars["MilesPower"].BaseValue,
-            Owner.Creature, this);
             await PowerCmd.Apply<VulnerablePower>(choiceContext, Owner.Creature, DynamicVars["VulnerablePower"].BaseValue, Owner.Creature, this);
             await PowerCmd.Apply<ChargePower>(choiceContext, Owner.Creature, DynamicVars["ChargePower"].BaseValue, Owner.Creature, this);
             await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
@@ -41,7 +33,6 @@ namespace SwarmTheSpire.Cards
         protected override void OnUpgrade()
         {
             DynamicVars.Block.UpgradeValueBy(2m);
-            DynamicVars["MilesPower"].UpgradeValueBy(1m);
             DynamicVars["ChargePower"].UpgradeValueBy(1m);
         }
     }
