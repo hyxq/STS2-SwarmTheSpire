3203e4f9c87c325f1ceb62bf1a5f01126a755b76
幻雨心晴 <yuanshuhan80318@outlook.com>
2026-07-14 15:22:30 +0800

    refactor(BOOM): 重做BOOM效果 - 2c攻击牌,施加并翻倍/p,造成伤害获得金币

--- SwarmTheSpire/Cards/Rare/BOOM.cs
+++ SwarmTheSpire/Cards/Rare/BOOM.cs
@@ -10,42 +10,59 @@ using SwarmTheSpire.Powers;
 namespace SwarmTheSpire.Cards
 {
     public sealed class Boom()
-        : SwarmEvilPoolCard(2, CardType.Skill, CardRarity.Rare, TargetType.AllEnemies, true)
+        : SwarmEvilPoolCard(2, CardType.Attack, CardRarity.Rare, TargetType.AllEnemies, true)
     {
         public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
-
-        public override bool GainsBlock => true;
 
         protected override IEnumerable<IHoverTip> AdditionalHoverTips =>
             [HoverTipFactory.FromPower<MilesPower>()];
 
         protected override IEnumerable<DynamicVar> CanonicalVars =>
-            [new PowerVar<MilesPower>(2m)];
+            [new PowerVar<MilesPower>(1m)];
 
         protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
         {
             var combatState = CombatState;
             ArgumentNullException.ThrowIfNull(combatState);
-            await PowerCmd.Apply<MilesPower>(choiceContext, combatState.HittableEnemies,
-                DynamicVars["MilesPower"].BaseValue, Owner.Creature, this);
 
-            var totalMiles = combatState.Enemies
-                .Where(c => c.IsAlive)
-                .Sum(c => c.GetPowerAmount<MilesPower>());
+            // Give 1(2) MilesPower to all enemies
+            var enemies = combatState.HittableEnemies.Where(c => c.IsAlive).ToList();
+            foreach (var enemy in enemies)
+            {
+                await PowerCmd.Apply<MilesPower>(choiceContext, enemy,
+                    DynamicVars["MilesPower"].BaseValue, Owner.Creature, this);
+            }
 
+            // Double the MilesPower stacks on all enemies
+            foreach (var enemy in enemies)
+            {
+                var currentAmount = enemy.GetPowerAmount<MilesPower>();
+                if (currentAmount > 0)
+                {
+                    await PowerCmd.Apply<MilesPower>(choiceContext, enemy,
+                        currentAmount, Owner.Creature, this);
+                }
+            }
+
+            // Deal damage equal to total MilesPower across all enemies
+            var totalMiles = enemies.Sum(c => c.GetPowerAmount<MilesPower>());
             if (totalMiles > 0)
             {
                 var val = await DamageCmd.Attack(totalMiles).FromCard(this)
                     .TargetingAllOpponents(combatState)
                     .Execute(choiceContext);
 
-                var actualDamage = val.Results.SelectMany(static r => r).Sum(static r => r.TotalDamage + r.OverkillDamage);
+                var actualDamage = val.Results.SelectMany(static r => r)
+                    .Sum(static r => r.TotalDamage + r.OverkillDamage);
 
                 // Gain gold equal to actual damage dealt
                 await PlayerCmd.GainGold(actualDamage, Owner);
             }
         }
 
-        protected override void OnUpgrade() => RemoveKeyword(CardKeyword.Exhaust);
+        protected override void OnUpgrade()
+        {
+            DynamicVars["MilesPower"].UpgradeValueBy(1m);
+        }
     }
 }
